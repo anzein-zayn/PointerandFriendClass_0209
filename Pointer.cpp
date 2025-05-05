657d588dd89fee1e512d46b485387ecb223d8cde
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2025-05-05 18:33:26 +0700

    Membuat derefrence

--- Pointer.cpp
+++ Pointer.cpp
@@ -1,2 +1,15 @@
 #include<iostream>
 using namespace std;
+
+int main ()
+{
+    //derefrence
+    int a = 10;
+    int b = 6;
+    int y = 20;
+    int *c = &a;
+    cout << " Alamat Memori : " << c << endl;
+    cout << " Nilai dari C : " << *c << endl;
+    c = &y;
+    cout << " Alamat Memori : " << c << endl;
+    cout << " Nilai dari C : " << *c << endl;
